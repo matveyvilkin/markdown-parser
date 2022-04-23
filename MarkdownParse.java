647f1c84256d3fe9dd0c450146475ed700a7ee6a
matveyvilkin <103228662+matveyvilkin@users.noreply.github.com>
2022-04-23 15:16:29 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,7 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            toReturn.add(markdown.substring(openParen+1, closeParen));
             currentIndex = closeParen + 1;
         }
 
