b234528eccd24ee295f8ceb48d8cf3f256916bb1
matveyvilkin <103228662+matveyvilkin@users.noreply.github.com>
2022-04-23 16:08:40 -0700

    MarkdownParse Bug #2 Fix
    Ensures that program first checks if there any COMPLETE link before running another iteration of while loop.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,7 +12,10 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            if (markdown.indexOf("[", currentIndex)==-1){
+            if (markdown.indexOf("[", currentIndex)==-1 || 
+                  markdown.indexOf("]", currentIndex)==-1 || 
+                  markdown.indexOf("(", currentIndex)==-1 || 
+                  markdown.indexOf(")", currentIndex)==-1 ) {
                 break;
             }
             int openBracket = markdown.indexOf("[", currentIndex);
